8c5fd2d65022b22ae26aa21f9afb31ecb8b57492
Asad9340 <asadulimran1999@gmail.com>
2024-03-23 00:43:18 +0600

    added navbar and banner and also footer section added successfully

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,38 @@
+import bannerImg from '../../assets/images/user.png'
+function Banner() {
+  return (
+    <section className="bg-[#7E90FE0D] text-gray-800">
+      <div className="container flex flex-col justify-center mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between max-w-5xl">
+        <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
+          <h1 className="text-5xl font-bold leading-none sm:text-6xl">
+            On Step <br /> Closer To Your <br />
+            <span className="text-violet-400">Dream Job</span>
+          </h1>
+          <p className="mt-6 mb-8 text-lg sm:mb-12">
+            Explore thousands of job opportunities with all the information you
+            need. Its your future. Come find it. Manage all your job application
+            from start to finish.
+          </p>
+          <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
+            <a
+              rel="noopener noreferrer"
+              href="#"
+              className="px-8 py-3 text-lg font-semibold rounded-lg bg-violet-400 hover:bg-violet-500 active:bg-violet-400 text-white"
+            >
+              Get Started
+            </a>
+          </div>
+        </div>
+        <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+          <img
+            src={bannerImg}
+            alt=""
+            className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128"
+          />
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default Banner;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,63 @@
+import { NavLink } from "react-router-dom";
+function Navbar() {
+  return (
+    <header className="p-4 text-gray-800 bg-[#7E90FE0D]">
+      <div className="container flex items-center justify-between mx-auto max-w-5xl">
+        <h2 className="text-xl md:text-2xl font-semibold">CareerHub</h2>
+        <ul className="items-stretch hidden space-x-3 lg:flex">
+          <li className="flex"></li>
+          <li className="flex">
+            <NavLink
+              rel="noopener noreferrer"
+              href="#"
+              className="flex items-center px-4 -mb-1 border-b-2 border-transparent"
+            >
+              Statistics
+            </NavLink>
+          </li>
+          <li className="flex">
+            <NavLink
+              rel="noopener noreferrer"
+              href="#"
+              className="flex items-center px-4 -mb-1 border-b-2 border-transparent"
+            >
+              Blog
+            </NavLink>
+          </li>
+          <li className="flex">
+            <NavLink
+              rel="noopener noreferrer"
+              href="#"
+              className="flex items-center px-4 -mb-1 border-b-2 border-transparent"
+            >
+              Applied Jobs
+            </NavLink>
+          </li>
+        </ul>
+        <div className="items-center flex-shrink-0 hidden lg:flex">
+          <button className="self-center px-8 py-3 font-semibold rounded-lg bg-violet-400 text-white hover:bg-violet-500 active:bg-violet-400">
+            Sign up
+          </button>
+        </div>
+        <button className="p-4 lg:hidden">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+            className="w-6 h-6 text-gray-100"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h16M4 18h16"
+            ></path>
+          </svg>
+        </button>
+      </div>
+    </header>
+  );
+}
+
+export default Navbar;
